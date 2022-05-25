a119a80ecd0cf414e857a48be440ca561824ce85
Martin Cavil <martincavil.dev@gmail.com>
2022-05-25 18:09:24 +0200

    creates portfolio compenen

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,64 @@
-import './portfolio.scss'
+import { useEffect, useState } from 'react';
+import './portfolio.scss';
+import PortfolioList from "../portfolioList/PortfolioList";
+import { webPortfolioList, mobilePortfolioList, contentPortolfioList, contentPortfolioList } from '../../data';
 
 export default function Portfolio() {
+
+  const [selected, setSelected] = useState("web");
+  const[data, setData] = useState([]);
+  const list = [
+    {
+      id: "web",
+      title: "Web App"
+    },
+    {
+      id: "mobile",
+      title: "Mobile App"
+    },
+    {
+      id: "content",
+      title: "Content"
+    }
+  ];
+
+  useEffect(()=> {
+    switch(selected) {
+      case "web":
+      setData(webPortfolioList);
+      break;
+      case "mobile":
+      setData(mobilePortfolioList);
+      break;
+      case "content":
+      setData(contentPortfolioList);
+      break;
+      default:
+        setData(webPortfolioList)
+    }
+  },[selected])
+
   return (
     <div className='portfolio' id="portfolio">
-
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map((item)=> (
+          <PortfolioList
+          title={item.title}
+          active={selected === item.id}
+          setSelected={setSelected}
+          id={item.id}
+          />
+        ))}
+      </ul>
+      <div className="container">
+        {data.map((d) => (
+           <div className="item">
+           <img src={d.img} alt="" />
+           <h3>{d.title}</h3>
+         </div>
+        ))}
+      </div>
     </div>
   )
 }
---
+++ src/data.js
@@ -0,0 +1,73 @@
+export const webPortfolioList = [
+  {
+    id: 1,
+    title: "Milk Project",
+    img: "https://cdn.dribbble.com/users/7072315/screenshots/18077774/media/56aef029b88b90972281937eef5e8087.png?compress=1&resize=1000x750&vertical=top"
+  },
+
+  {
+    id: 2,
+    title: "NFT Landing Page",
+    img: "https://cdn.dribbble.com/users/6047818/screenshots/17011534/media/abaafd023c9c2c526cae20aaeb4a33a6.png?compress=1&resize=1000x750&vertical=top"
+  },
+
+  {
+    id: 3,
+    title: "Sneakers App",
+    img: "https://cdn.dribbble.com/users/2064121/screenshots/15595531/media/0aa6f100d6dc369e5524f4aa14b7dcec.png?compress=1&resize=1200x900&vertical=top"
+  },
+
+  {
+    id: 4,
+    title: "Netflix App",
+    img: "https://cdn.dribbble.com/users/3506230/screenshots/16626652/media/2d756a0f3075734a3e35c8f1204f9bed.png?compress=1&resize=1200x900&vertical=top"
+  },
+
+  {
+    id: 5,
+    title: "Music App",
+    img: "https://cdn.dribbble.com/users/8526939/screenshots/17856872/media/12784b8df8ab773613c2c735eaf67a78.jpg?compress=1&resize=1200x900&vertical=top"
+  }
+]
+
+export const mobilePortfolioList = [
+  {
+    id: 1,
+    title: "Milk Project",
+    img: "https://cdn.dribbble.com/users/7072315/screenshots/18077774/media/56aef029b88b90972281937eef5e8087.png?compress=1&resize=1000x750&vertical=top"
+  },
+
+  {
+    id: 2,
+    title: "NFT Landing Page",
+    img: "https://cdn.dribbble.com/users/6047818/screenshots/17011534/media/abaafd023c9c2c526cae20aaeb4a33a6.png?compress=1&resize=1000x750&vertical=top"
+  },
+
+  {
+    id: 3,
+    title: "Sneakers App",
+    img: "https://cdn.dribbble.com/users/2064121/screenshots/15595531/media/0aa6f100d6dc369e5524f4aa14b7dcec.png?compress=1&resize=1200x900&vertical=top"
+  },
+
+  {
+    id: 4,
+    title: "Netflix App",
+    img: "https://cdn.dribbble.com/users/3506230/screenshots/16626652/media/2d756a0f3075734a3e35c8f1204f9bed.png?compress=1&resize=1200x900&vertical=top"
+  }
+
+]
+
+export const contentPortfolioList = [
+  {
+    id: 1,
+    title: "Milk Project",
+    img: "https://cdn.dribbble.com/users/7072315/screenshots/18077774/media/56aef029b88b90972281937eef5e8087.png?compress=1&resize=1000x750&vertical=top"
+  },
+
+  {
+    id: 2,
+    title: "NFT Landing Page",
+    img: "https://cdn.dribbble.com/users/6047818/screenshots/17011534/media/abaafd023c9c2c526cae20aaeb4a33a6.png?compress=1&resize=1000x750&vertical=top"
+  }
+
+]
