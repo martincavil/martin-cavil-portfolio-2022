18665c6e452161a1c026ab73097e8ffbc0635625
Martin Cavil <martincavil.dev@gmail.com>
2022-05-26 12:59:50 +0200

    created card

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -3,7 +3,26 @@ import "./testimonials.scss"
 export default function Testimonials() {
   return (
     <div className="testimonials" id="testimonials">
-
+      <h1>Testimonials</h1>
+      <div className="container">
+      <div className="card">
+        <div className="top">
+          <img src="assets/arrow.png" className="left" alt="" />
+          <img src="assets/victor.png" className="userPicture" alt="" />
+          <img src="assets/linkedin.png" className="socialMedia" alt="" />
+        </div>
+        <div className="center">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Eveniet vel magni maxime provident cum.
+            Quo similique eius beatae, consectetur iure in maxime doloremque nulla,
+            ipsum dolorum mollitia omnis, quidem possimus?</p>
+        </div>
+        <div className="bottom">
+          <h3>Victor Villemin</h3>
+          <h3>CEO of Art Gallery</h3>
+        </div>
+      </div>
+      </div>
     </div>
   )
 }
