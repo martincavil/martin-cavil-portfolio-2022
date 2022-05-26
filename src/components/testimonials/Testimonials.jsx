f88d381a901a382f51d3ca0ea9d7498807a839d3
Martin Cavil <martincavil.dev@gmail.com>
2022-05-26 13:34:27 +0200

    added pictures in testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -7,24 +7,24 @@ export default function Testimonials() {
       id: "1",
       name: "Victor Villemin",
       title: "CEO of Art Gallery",
-      img: "assets/victor.png",
+      img: "https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870",
       icon: "assets/linkedin.png",
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem voluptatum id natus ad necessitatibus, cum omnis aliquid sint culpa qui voluptates ipsum eius tenetur adipisci non rerum sunt exercitationem nisi!"
     },
     {
       id: "2",
-      name: "Victor Villemin",
-      title: "CEO of Art Gallery",
-      img: "assets/victor.png",
-      icon: "assets/linkedin.png",
+      name: "@JohnDoe",
+      title: "CEO of JD Corps.",
+      img: "https://images.unsplash.com/photo-1583136809091-ba7c0e8c6f75?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870",
+      icon: "assets/twitter.png",
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem voluptatum id natus ad necessitatibus, cum omnis aliquid sint culpa qui voluptates ipsum eius tenetur adipisci non rerum sunt exercitationem nisi!",
       featured: true
     },
     {
       id: "3",
-      name: "Victor Villemin",
-      title: "CEO of Art Gallery",
-      img: "assets/victor.png",
+      name: "Emma P.",
+      title: "UI Designer",
+      img: "https://images.unsplash.com/photo-1542981533-e288c4fda66d?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=874",
       icon: "assets/linkedin.png",
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem voluptatum id natus ad necessitatibus, cum omnis aliquid sint culpa qui voluptates ipsum eius tenetur adipisci non rerum sunt exercitationem nisi!"
     },
